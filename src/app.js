1cd18f101ee8f96de8a27223569266d9aaa303dc
Angel Lizardo <aahlizardo.britanico@gmail.com>
2024-12-04 21:40:58 -0600

    Agregar nodemon y comentarios No hice mucho en este commit puesto que me faltaba instalar mongodb, pero agregué algunos comentarios.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@
 // Importar la libreria express
 import express from 'express';
 
+// Importar la libreria morgan. Es un logger para visualizar peticiones la servidor
+import morgan from 'morgan';
+
 // Esta constante es básicamente la que funge como servidor
 const app = express();
 
+// Se le especifica a la aplicación el que se usará el módulo morgan en modo 'dev'
+app.use(morgan('dev'));
+
+// Esta línea permite a otros archivos importar la variable app
 export default app;
